95f0570d52a0702532c010748ea6956efdadc05c
Noah Hradek <noahh717@gmail.com>
2013-06-05 13:41:03 -0600

    Updated src and bin

--- src/org/vm/Assembler.java
+++ src/org/vm/Assembler.java
@@ -1,5 +1,22 @@
-package org.vm;
+/*
+ Assembler.java Assembler for bytecode
+    Copyright (C) 2013  Lord Voldemort Tyrannus
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.vm;
 import java.util.*;
 public class Assembler {
 
@@ -37,10 +54,13 @@ public class Assembler {
 				else if(l[0].equalsIgnoreCase("LMA")) a[0] = 12;
 				else if(l[0].equalsIgnoreCase("LMB")) a[0] = 13;
 				else if(l[0].equalsIgnoreCase("LMO")) a[0] = 14;
+				else if(l[0].equalsIgnoreCase("JMP")) a[0] = 16;
+				else if(l[0].equalsIgnoreCase("CIN")) a[0] = 17;
 				a[1] = Integer.parseInt(l[1]);
 			}
 			else{
 				if(l[0].equalsIgnoreCase("FLO")){a[0] = 10; a[1] = null;}
+				else if(l[0].equalsIgnoreCase("CMP")){a[0] = 15; a[1] = null;}
 			}
 			ls.add(a);
 		}
